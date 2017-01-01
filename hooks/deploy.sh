8f1c6abd9035db502edce4f40dd3e4a0eb0dffc1
AP Ljungquist <anderspetter.ljungquist@gmail.com>
2017-01-01 13:44:52 +0000

    Hooks deployment script can be called from anywhere but will fail if not called from within a git repository. Also more verbose

--- hooks/deploy.sh
+++ hooks/deploy.sh
@@ -1,21 +1,25 @@
-# Automatically add actions to hooks. 
-# To be executed from the directory in which the it and the actions reside.
-root_dir="$(git rev-parse --show-toplevel)"
+# Automatically add actions to hooks, e.g. after cloning repository. 
+git_dir="$(git rev-parse --show-toplevel)/.git" || exit
 
+action_dir="$(dirname $0)"
 action_filename="update-head-info.sh"
-for hook_name in "post-commit" "post-checkout" "post-merge"; do
-	hook_filepath="${root_dir}/.git/hooks/${hook_name}"
+action_filepath="${action_dir}/${action_filename}"
+
+for hook_filename in "post-commit" "post-checkout" "post-merge"; do
+	hook_filepath="${git_dir}/hooks/${hook_filename}"
 	
 	if [ ! -e "${hook_filepath}" ]; then
+		echo "Creating hook ${hook_filepath}"
 		echo "#!/bin/sh" >> "${hook_filepath}"
 		chmod +x "${hook_filepath}"
 	fi
 	
-	if grep -Fq "${action_filename}" "${hook_filepath}"; then
-		echo "${action_filename} already deployed to ${hook_name}"
+	if grep -Fq "${action_filename}" "${hook_filepath}"; then 
+		echo "${action_filename} already deployed to ${hook_filepath}"
 	else
+		echo "Deploying ${action_filename} to ${hook_filepath}"
 		echo "# Automatic deployment of ${action_filename}" >> "${hook_filepath}"
-		cat "${action_filename}" >> "${hook_filepath}"
+		cat "${action_filepath}" >> "${hook_filepath}"
 
 	fi
 done
